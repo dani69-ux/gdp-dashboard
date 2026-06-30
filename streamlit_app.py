e8462c5035ac6d0d790cb83b19621f64abf023bc
dani69-ux <danibernabe@me.com>
2026-06-30 13:15:39 +0000

    fixed error "response is not defoned"

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,29 +31,30 @@ if user_input:
     # I-display agad ang tinype ng user at i-save sa history
     with st.chat_message("user"):
         st.write(user_input)
-        st.session_state.chat_history.append({"role": "user", "text": user_input})
+    st.session_state.chat_history.append({"role": "user", "text": user_input})
 
-        # I-format ang buong history para ipadala kay Gemini
-        api_contents =[]
-        for msg in st.session_state.chat_history: 
-            api_contents.append(f"{msg['role']}: {msg['text']}")
+    # I-format ang buong history para ipadala kay Gemini
+    api_contents =[]
+    for msg in st.session_state.chat_history: 
+        api_contents.append(f"{msg['role']}: {msg['text']}")
 
-        api_contents.append("assistant: ") 
+    api_contents.append("assistant: ") 
 
-        try:
-            # 1. Tawagan si Gemini gamit ang buong history ng usapan
-            respond = client.models.generate_content(
-                model='gemini-2.5-flash', 
-                contents="\n".join(api_contents), 
-            )
-            # 2. DAPAT NASA LOOB ITO NG TRY (may 8 spaces o dalawang tab mula sa kaliwa)
-            # I-display ang sagot ni Wizard AI at i-save sa history
-            with st.chat_message("assistant", avatar="🧙‍♂️"): 
-                st.write(response.text) 
-            st.session_state.chat_history.append({"role": "assistant", "tetxt": response.text}) 
+    try:
+        # 1. Tawagan si Gemini gamit ang buong history ng usapan
+        respond = client.models.generate_content(
+            model='gemini-2.5-flash', 
+            contents="\n".join(api_contents), 
+        )
 
-        except Exception as e:
-            # 3. Kapag nagka-error si Gemini, dito siya tatalon at ligtas ang app
-            st.error("🚨 Error Wizard AI.") 
-            st.warning(f"Detalye ng problema: {e}") 
+        # 2. DAPAT NASA LOOB ITO NG TRY (may 8 spaces o dalawang tab mula sa kaliwa)
+        # I-display ang sagot ni Wizard AI at i-save sa history
+        with st.chat_message("assistant", avatar="🧙‍♂️"): 
+            st.write(response.text) 
+        st.session_state.chat_history.append({"role": "assistant", "tetxt": response.text}) 
+
+    except Exception as e:
+        # 3. Kapag nagka-error si Gemini, dito siya tatalon at ligtas ang app
+        st.error("🚨 Error Wizard AI.") 
+        st.warning(f"Detalye ng problema: {e}") 
 
